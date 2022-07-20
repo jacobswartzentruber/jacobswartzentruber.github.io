f3a3fc5a22b2ef254ad47654603a42aac866b9a7
jacob.swartzentruber <jsswart@gmail.com>
2022-07-20 10:35:44 -0500

    Added styling, content and pictures

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
-# developer-portfolio
- Portfolio for Web Development
+# Jacob Swartzentruber - Developer Portfolio
+ Welcome to my static-site Web Development Portfolio!
+
+ A live version of this site is hosted on github pages: [jacobswartzentruber.github.io](https://jacobswartzentruber.github.io).
+
+ If you have questions or would like to contact me, please reach out through the contact links provided in the portfolio or message me on linked in!
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,17 +1,23 @@
+const burger = document.querySelector("#burger-menu");
+const navigation = document.querySelector(".navigation");
+const navbar = document.querySelector("nav");
+const navLinks = document.querySelectorAll(".nav-link");
+
 let lastScrollPosition = 0;
 let lastDirection = "up";
 
 //Hide and Reveal Navbar when scrolling
 document.addEventListener('scroll', function(e) {
+    //If on mobile screen, hide navbar
+    navigation.classList.remove("show");
+
     let newScrollPosition = window.scrollY;
     
     if(lastDirection != "down" && newScrollPosition > lastScrollPosition){
-        let d = document.querySelector('nav');
-        d.style.top = (-d.offsetHeight-10) + "px";
+        navbar.style.top = (-navbar.offsetHeight-10) + "px";
         lastDirection = "down";
     }else if(lastDirection != "up" && newScrollPosition < lastScrollPosition){
-        let d = document.querySelector('nav');
-        d.style.top = "0px";
+        navbar.style.top = "0px";
         lastDirection = "up";
     }
 
@@ -19,13 +25,16 @@ document.addEventListener('scroll', function(e) {
 });
 
 //Smooth scrolling for nav links that accomodates nav-bar height
-document.querySelectorAll(".nav-link").forEach(item => {
-    item.addEventListener("click", (e) => {
+navLinks.forEach(link => {
+    link.addEventListener("click", (e) => {
+        //If on mobile screen, hide navbar
+        navigation.classList.remove("show");
+
         let sectionId = e.target.id.slice(4);
         let projectsY = document.querySelector("#"+sectionId).getBoundingClientRect().top;
         let scrollTop = window.pageYOffset || document.documentElement.scrollTop;
         let scrollAmount = projectsY + scrollTop;
-        if (projectsY < 0) scrollAmount -= document.querySelector("nav").offsetHeight;
+        if (projectsY < 0) scrollAmount -= navbar.offsetHeight;
 
         window.scrollTo({
             top: scrollAmount,
@@ -33,4 +42,9 @@ document.querySelectorAll(".nav-link").forEach(item => {
             behavior: "smooth"
         })
     });
+});
+
+//Toggle nav bar links when burger menu clicked
+burger.addEventListener("click", (e) => {
+    navigation.classList.toggle("show");
 });
